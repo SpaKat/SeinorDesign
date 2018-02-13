215f30f8d064acfa534e95ec0a9c01e6a250c4d2
Laga-Sama <Laga-Sama@SpaKitten>
2018-02-13 15:11:34 -0500

    Create new mission and DataGpsComp

---
+++ GoodOldSensorDesgin/CreateNewMission.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class CreateNewMission extends MenuItem {
+	
+	MissionSelect missionCharts;
+
+	public CreateNewMission(MissionSelect missionCharts) {
+		this.missionCharts = missionCharts;
+		setText("Create Mission");
+		setOnAction(newMission ->{
+			createMission(missionCharts.getSelectThemission());
+		});	}
+
+	private void createMission(ArrayList<UavMission> selectThemission) {
+		Stage stage = new Stage();
+		stage.setTitle("Create Mission");
+		VBox vbox = new VBox();
+		
+		Text askForMissionName = new Text("Enter the Mission Name");
+		Text askForMissionID = new Text("Enter the Mission ID");
+		
+		TextField enterMissionName = new TextField();
+		TextField enterMissionID = new TextField();
+		
+		Button enter = new Button("Enter");
+		vbox.getChildren().addAll(askForMissionName,enterMissionName,askForMissionID,enterMissionID,enter);
+
+		enter.setOnAction(ent ->{
+			UavMission uavMission = new UavMission(enterMissionName.getText(), enterMissionID.getText());
+			selectThemission.add(uavMission);
+			missionCharts.setUavMission(uavMission);
+			missionCharts.refresh();
+			stage.close();
+		});
+		
+		Scene scene = new Scene(vbox);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
